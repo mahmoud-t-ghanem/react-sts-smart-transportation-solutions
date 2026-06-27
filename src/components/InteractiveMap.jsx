006d4e0657d8201174420bc7da11588fe7b10896
mahmoud-t-ghanem <mahmoud.taha.ghanem@gmail.com>
2026-06-27 20:00:17 +0300

    sts-v1.0.2

--- src/components/InteractiveMap.jsx
+++ src/components/InteractiveMap.jsx
@@ -11,10 +11,8 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 const redIcon = new L.Icon({
-  iconUrl:
-    "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+  iconUrl: "/images/marker-icon.png",
+  shadowUrl: "/images/marker-shadow.png",
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
